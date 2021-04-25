de6e5790bd2e3cdfe4a1ff35ddc1ecce425fe3ef
Quốc Thắng <quocthang036@gmail.com>
2021-04-26 03:12:28 +0700

    fix conflict 5

--- Care_Management_and_Private_Parking/Care_Management_and_Private_Parking/4.Calendar DOW/CalendarDOWForm.cs
+++ Care_Management_and_Private_Parking/Care_Management_and_Private_Parking/4.Calendar DOW/CalendarDOWForm.cs
@@ -12,140 +12,26 @@ namespace Care_Management_and_Private_Parking
 {
     public partial class CalendarDOWForm : Form
     {
-        #region Properties
-        private List<List<Button>> matrix;                                          //Khởi tạo mảng 2 chiều mang giá trị là các nút
-
-        public List<List<Button>> Matrix
-        {
-            get { return matrix; }
-            set { matrix = value; }
-        }
-
-        private List<string> dayOfWeek = new List<string>{ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
-        #endregion
-
         public CalendarDOWForm()
         {
             InitializeComponent();
             LoadMatrixDay();
         }
-        void SetDefaultDay()
-        {
-            dateTime.Value = DateTime.Now;                                           //Set giá trị ngày tháng của datetimepicker thành ngày hôm nay
-        }
-        //Tạo ra ma trận nút 7x6 (chưa hiển thị ngày)
         void LoadMatrixDay()
         {
-            Matrix = new List<List<Button>>();
             Button oldbtn = new Button() { Width = 0, Height = 0, Location = new Point(-Variable.margin, 0) };
             for(int i = 0; i < Variable.DayOfColumn; ++i)
             {
-                Matrix.Add(new List<Button>());
                 for(int j = 0; j < Variable.DayOfWeeks; ++j)
                 {
                     Button btn = new Button() {Width = Variable.btnWidth, Height = Variable.btnHeight};
                     btn.Location = new Point(oldbtn.Location.X + oldbtn.Width + Variable.margin, oldbtn.Location.Y);
                     pnlMatrixDay.Controls.Add(btn);
-                    Matrix[i].Add(btn);
                     oldbtn = btn;
                 }
                 oldbtn = new Button() { Width = 0, Height = 0, Location = new Point(-Variable.margin, oldbtn.Location.Y + Variable.btnHeight) };
             }
-            SetDefaultDay();
         }
 
-        int DayOfMonth(DateTime date)
-        {
-            switch(date.Month)
-            {
-                case 1:
-                case 3:
-                case 5:
-                case 7:
-                case 8:
-                case 10:
-                case 12:
-                    return 31;
-                case 2:
-                    if ((date.Year % 4 == 0 && date.Year % 100 != 0) || date.Year % 400 == 0)
-                        return 29;
-                    else
-                        return 28;
-                default:
-                    return 30;
-            }
-        }                                            //Trả về số ngày tương ứng với tháng
-        bool IsEqualDay(DateTime dateA, DateTime dateB)
-        {
-            return dateA.Day == dateB.Day && dateA.Month == dateB.Month && dateA.Year == dateB.Year;
-        }
-        //Thêm ngày vào nút
-        void AddNumberIntoMatrixDay(DateTime date)
-        {
-            ClearMatrix();
-            DateTime useDate = new DateTime(date.Year, date.Month, 1);               //Trả về ngày đầu tiên của tháng được nhập vào (ở dây là tháng hiện tại do datetimepicker)
-            int line = 0;
-            for(int i = 1; i <= DayOfMonth(date); ++i)
-            {
-                int column = dayOfWeek.IndexOf(useDate.DayOfWeek.ToString());        //ví dụ: trả về Thursday -> index = 4
-                Button btn = Matrix[line][column];
-                btn.Text = i.ToString();
-
-                if(IsEqualDay(useDate, DateTime.Now))                                //Ngày hôm nay sẽ được bôi vàng
-                {
-                    btn.BackColor = Color.Yellow;
-                }
-
-                if (IsEqualDay(useDate, date))                                       //Ngày được chọn trên datetimepicker sẽ được bôi màu
-                {
-                    btn.BackColor = Color.LightGreen;
-                }
-
-                if (column >= 6)                                                     //Cuối tuần ( 0 1 2 3 4 5 6 )
-                    line++;
-
-                useDate = useDate.AddDays(1);                                        //+ thêm 1 ngày vào ngày đầu tiên -> trở thành ngày tiếp theo
-
-            }
-        }
-
-        void ClearMatrix()                                                           //Hàm xóa đi các giá trị trong mảng nút
-        {
-            for(int i = 0; i < Matrix.Count; ++i)
-            {
-                for(int j = 0; j < Matrix[i].Count; ++j)
-                {
-                    Button btn = Matrix[i][j];
-                    btn.Text = "";
-                    btn.BackColor = DefaultBackColor;
-                }
-            }
-        }
-        
-        private void dateTime_ValueChanged(object sender, EventArgs e)
-        {
-            AddNumberIntoMatrixDay((sender as DateTimePicker).Value);
-        }
-        //------------------------------------------------------------------------------------------------------------//
-        //Các nút khác
-        private void btnToday_Click(object sender, EventArgs e)
-        {
-            SetDefaultDay();
-        }
-
-        private void btnExit_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void btnPreviousMonth_Click(object sender, EventArgs e)
-        {
-            dateTime.Value = dateTime.Value.AddMonths(-1);
-        }
-
-        private void btnNextMonth_Click(object sender, EventArgs e)
-        {
-            dateTime.Value = dateTime.Value.AddMonths(1);
-        }
     }
 }
