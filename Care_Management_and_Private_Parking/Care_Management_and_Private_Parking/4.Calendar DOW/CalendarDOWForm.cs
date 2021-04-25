e8f7b722abbea4e30f3c40543629e90a80560aed
Quốc Thắng <quocthang036@gmail.com>
2021-04-25 19:10:42 +0700

    Calendar Form
    Edit Calendar Form

---
+++ Care_Management_and_Private_Parking/Care_Management_and_Private_Parking/4.Calendar DOW/CalendarDOWForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Care_Management_and_Private_Parking
+{
+    public partial class CalendarDOWForm : Form
+    {
+        public CalendarDOWForm()
+        {
+            InitializeComponent();
+            LoadMatrixDay();
+        }
+        void LoadMatrixDay()
+        {
+            Button oldbtn = new Button() { Width = 0, Height = 0, Location = new Point(-Variable.margin, 0) };
+            for(int i = 0; i < Variable.DayOfColumn; ++i)
+            {
+                for(int j = 0; j < Variable.DayOfWeeks; ++j)
+                {
+                    Button btn = new Button() {Width = Variable.btnWidth, Height = Variable.btnHeight};
+                    btn.Location = new Point(oldbtn.Location.X + oldbtn.Width + Variable.margin, oldbtn.Location.Y);
+                    pnlMatrixDay.Controls.Add(btn);
+                    oldbtn = btn;
+                }
+                oldbtn = new Button() { Width = 0, Height = 0, Location = new Point(-Variable.margin, oldbtn.Location.Y + Variable.btnHeight) };
+            }
+        }
+
+    }
+}
---
+++ Care_Management_and_Private_Parking/Care_Management_and_Private_Parking/Variable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Care_Management_and_Private_Parking
+{
+    class Variable
+    {
+        public static int DayOfWeeks = 7;               //Ngày trong tuần
+        public static int DayOfColumn = 6;              //6 hàng
+        public static int btnWidth = 76;                //Độ rộng của nút ngày
+        public static int btnHeight = 30;               //Độ dài của nút ngày
+        public static int margin = 5;                   //Khoảng cách giãn cách giữa các nút ngày
+    }
+}
