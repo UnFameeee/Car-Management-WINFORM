a887d6ac4ab065ba3897f7b543a18e76282eb5ba
Ng. Hoang Vu <vu2872001@gmail.com>
2021-04-26 11:26:56 +0700

    fix manageform

--- Care_Management_and_Private_Parking/Care_Management_and_Private_Parking/3.Employee/ManangeForm.cs
+++ Care_Management_and_Private_Parking/Care_Management_and_Private_Parking/3.Employee/ManangeForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -18,26 +19,57 @@ namespace Care_Management_and_Private_Parking
         }
 
         Employee emp = new Employee();
+        MY_DB db = new MY_DB();
+        void reloadDatagridview()
+        {
+            SqlCommand command = new SqlCommand("Select * from EMPLOYEE", db.getConnection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dgvEmp.DataSource = table;
+            dgvEmp.AllowUserToAddRows = false;
+            dgvEmp.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.DisplayedCells);
+        }
+
+        bool verif()
+        {
+            if (tbEmpID.Text.Trim() == ""
+                || tbFullName.Text.Trim() == ""
+                || tbPhone.Text.Trim() == ""
+                || tbIdentity.Text.Trim() == ""
+                || tbJobID.Text.Trim() == ""
+                || tbShiftID.Text.Trim() == "")
+                return false;
+
+            else return true;
+        }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
             string EmpID = tbEmpID.Text;
-            string FName = tbFullName.Text;
-            string Gender = "Male";
+            string FName = tbFullName.Text;            
             string Phone = tbPhone.Text;
             string Identity = tbIdentity.Text;
             int JobID = Convert.ToInt32(tbJobID.Text);
             int ShiftID = Convert.ToInt32(tbShiftID.Text);
+            string Gender = "Male";
 
-            if (EmpID.Trim() == "")
+            if (rdbtnFemale.Checked)
+                Gender = "Female";
+
+            if (verif())
             {
-                MessageBox.Show("Add an EmpID", "Add Employee", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-            else if (emp.checkEmp(EmpID))
-            {
-                if (emp.insertEmployee(EmpID, FName, Gender, Phone, Identity, JobID, ShiftID))
+                if (emp.checkEmp(EmpID))
                 {
-                    MessageBox.Show("New Employee Inserted", "Add Employee", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    if (emp.insertEmployee(EmpID, FName, Gender, Phone, Identity, JobID, ShiftID))
+                    {
+                        MessageBox.Show("New Employee Inserted", "Add Employee", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        reloadDatagridview();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Employee Not Inserted", "Add Employee", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
                 }
                 else
                 {
@@ -46,14 +78,14 @@ namespace Care_Management_and_Private_Parking
             }
             else
             {
-                MessageBox.Show("Employee Not Inserted", "Add Employee", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("Add Employee's Information", "Add Employee", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
 
-        }
+        }        
 
-        private void tpEmployees_Enter(object sender, EventArgs e)
+        private void btnRefresh_Click(object sender, EventArgs e)
         {
-            
+            reloadDatagridview();
         }
     }
 }
