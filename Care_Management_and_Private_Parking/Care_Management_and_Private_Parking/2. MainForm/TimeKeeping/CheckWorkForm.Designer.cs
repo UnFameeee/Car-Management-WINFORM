2d8ad68ee0c7df0034c79cef86ec781a8f83a7c7
Quốc Thắng <quocthang036@gmail.com>
2021-05-23 00:46:12 +0700

    add timekeeping
    add timekeeping

---
+++ Care_Management_and_Private_Parking/Care_Management_and_Private_Parking/2. MainForm/TimeKeeping/CheckWorkForm.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace Care_Management_and_Private_Parking
+{
+    partial class CheckWorkForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CheckWorkForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(37)))), ((int)(((byte)(41)))));
+            this.ClientSize = new System.Drawing.Size(913, 712);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CheckWorkForm";
+            this.Text = "CheckWorkForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
--- Care_Management_and_Private_Parking/Care_Management_and_Private_Parking/2. MainForm/TimeKeeping/TimeKeeping.cs
+++ Care_Management_and_Private_Parking/Care_Management_and_Private_Parking/2. MainForm/TimeKeeping/TimeKeeping.cs
@@ -12,9 +12,56 @@ namespace Care_Management_and_Private_Parking
 {
     public partial class TimeKeeping : Form
     {
+        CheckWorkForm frmCheckWorkForm = new CheckWorkForm() { TopLevel = false, TopMost = false };
+        ManageShiftForm frmManageShiftForm = new ManageShiftForm() { TopLevel = false, TopMost = false };
+        Cursor cur1 = Cursors.Hand;
+        Cursor cur2 = Cursors.Default;
+
         public TimeKeeping()
         {
             InitializeComponent();
         }
+        void loadForm()
+        {
+            this.pnlMain.Controls.Add(frmCheckWorkForm);
+            this.pnlMain.Controls.Add(frmManageShiftForm);
+            tick();
+        }
+        private void tick()
+        {
+            //Chỉnh Checked
+            btnTimeKeeping.Checked = false;
+            btnStatistic.Checked = false;
+            btnWorkShift.Checked = false;
+            //Chỉnh Cursor
+            btnTimeKeeping.Cursor = cur1;
+            btnStatistic.Cursor = cur1;
+            btnWorkShift.Cursor = cur1;
+            //Tắt tất cả các form
+            frmCheckWorkForm.Hide();
+            frmManageShiftForm.Hide();
+        }
+
+        private void btnTimeKeeping_Click(object sender, EventArgs e)
+        {
+            tick();
+            btnTimeKeeping.Checked = true;
+            btnTimeKeeping.Cursor = cur2;
+            frmCheckWorkForm.Show();
+        }
+
+        private void btnWorkShift_Click(object sender, EventArgs e)
+        {
+            tick();
+            btnWorkShift.Checked = true;
+            btnWorkShift.Cursor = cur2;
+            frmManageShiftForm.Show();
+        }
+
+        private void btnStatistic_Click(object sender, EventArgs e)
+        {
+
+        }
+        
     }
 }
